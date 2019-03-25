e4f38f321a9110278bb5c41a157174767c2d1118
David Llop <931684+sembrestels@users.noreply.github.com>
2019-03-25 15:56:21 +0100

    Refactor background script to avoid async calls and to show errors

--- app/src/script.js
+++ app/src/script.js
@@ -6,32 +6,37 @@ const INITIALIZATION_TRIGGER = Symbol('INITIALIZATION_TRIGGER')
 
 const api = new AragonApi()
 
-api.store(
-  async (state, event) => {
-    let newState
+const reducer = (state, event) => {
+  let newState
+  const { count } = state
 
-    switch (event.event) {
-      case INITIALIZATION_TRIGGER:
-        newState = { count: await getValue() }
-        break
-      case 'Increment':
-        newState = { count: await getValue() }
-        break
-      case 'Decrement':
-        newState = { count: await getValue() }
-        break
-      default:
-        newState = state
-    }
+  switch (event.event) {
+    case INITIALIZATION_TRIGGER:
+      newState = { count: 0 }
+      break
+    case 'Increment':
+      newState = { count: count + parseInt(event.returnValues.step) }
+      break
+    case 'Decrement':
+      newState = { count: count - parseInt(event.returnValues.step) }
+      break
+    default:
+      newState = state
+  }
 
-    return newState
-  },
+  return newState
+}
+
+api.store((state, event) => {
+  try {
+    return reducer(state, event)
+  } catch(e) {
+    console.error(e)
+    throw e
+  }
+},
   [
     // Always initialize the store with our own home-made event
     of({ event: INITIALIZATION_TRIGGER }),
   ]
 )
-
-async function getValue() {
-  return parseInt(await api.call('value').toPromise(), 10)
-}
